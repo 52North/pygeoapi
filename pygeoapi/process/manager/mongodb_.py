6be232022f62af4118bd9c3bf24a2958142a3f81
Alexander Pilz <51150328+xcomagent95@users.noreply.github.com>
2022-12-06 15:43:23 +0100

    Added Manager and Processes
    Added the blueprint for the mongodb job-manager and two additional processes to test

---
+++ pygeoapi/process/manager/mongodb_.py
@@ -0,0 +1,52 @@
+#MongoDB Job-Manager
+from pygeoapi.process.manager.base import BaseManager
+from pygeoapi.util import JobStatus
+from pymongo import MongoClient
+
+class MongoDBManager(BaseManager):
+    def __init__(self, manager_def):
+        super().__init__(manager_def)
+        self.is_async = True
+
+    def _connect(self):
+        #connection in CFG would be something like mongodb://localhost:27017/
+        try:
+            client = MongoClient(self.connection)
+            self.db = client
+            return True
+        except:
+            return False
+
+    def destroy(self):
+        try:
+            self.db.close()
+            return True
+        except:
+            return False
+
+    def get_jobs(self, status=None):
+        try:
+            database = self.db.job_manager_pygeoapi
+            collection = database.jobs
+
+            return collection
+        except:
+            return False
+
+    def add_job(self, job_metadata):
+        pass
+
+    def update_job(self, job_id, update_dict):
+        pass
+
+    def delete_job(self, job_id):
+        pass
+
+    def get_job(self, job_id):
+        pass
+
+    def get_job_result(self, job_id):
+        pass
+
+    def __repr__(self):
+        return '<MongoDBManager> {}'.format(self.name)
